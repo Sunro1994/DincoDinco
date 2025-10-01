4025e5b0eaaf7e299f395ee2b73a1924ced9e863
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2025-10-02 03:34:55 +0900

    [level 2] Title: 가장 큰 수, Time: 1554.29 ms, Memory: 23 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,30 @@
+import functools
+
+def compare(x,y):
+    xy = x+y
+    yx = y+x
+
+    # yx가 더 크면 1
+    if yx> xy:
+        return 1
+    # 반대가 더 크면 -1
+    elif yx< xy:
+        return -1
+    # 같으면 0
+    else:
+        return 0
+
+
+def solution(numbers):
+    str_numbers = [str(x) for x in numbers]
+
+    str_numbers.sort(key = functools.cmp_to_key(compare))
+
+    answer = "".join(str_numbers)
+    
+    answer = str(int(answer))
+    
+    
+    return answer
+
+
