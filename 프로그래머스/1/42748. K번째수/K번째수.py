adf194384079597abfb24a307a8b2e8ba91b0a05
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2025-10-22 00:16:00 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,19 +1,7 @@
-# 1번쨰
-#  0 :1  2   3  4:  5 6
-# [1, 5, 2, 6, 3, 7, 4]
-# [5,2,6,3]
-# [2,3,5,6] -> 5
-
-# [ 1, 5, 2, 6, 3 ,7 , 4]
-# [ 1, 2, 3 , 4 5 6 7 ] -> 3
-
 def solution(array, commands):
     answer = []
-
     for command in commands:
-        limited_arr = array[command[0]-1:command[1]]
-        limited_arr.sort()
-        answer.append(limited_arr[command[2]-1])
-
+        result = array[command[0]-1:(command[1])]
+        result.sort()
+        answer.append(result[command[2]-1])
     return answer
-
