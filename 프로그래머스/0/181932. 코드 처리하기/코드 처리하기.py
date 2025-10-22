ef48dad9fae0e93981ea2a1a822f9c9e868c479f
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2025-10-22 12:20:03 +0900

    [level 0] Title: 코드 처리하기, Time: 14.40 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,16 @@
+def solution(code):
+    ret = ""
+    mode = 0
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i] != "1" and i %2 ==0:
+                ret += code[i]
+            elif code[i] == "1":
+                mode = 1
+        else :
+            if code[i] != "1" and i %2 !=0:
+                ret += code[i]
+            elif code[i] =="1":
+                mode = 0
+
+    return "EMPTY" if ret =='' else ret
